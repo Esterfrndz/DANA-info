7999056624aa6bd7f2e37a2628848b36a175a983
Ester <esterffernandezz@gmail.com>
2024-11-03 14:47:58 +0100

    updated information

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -125,13 +125,6 @@ const townsData = [
   ];
   
 
-// Asegúrate de que Leaflet pueda cargar los iconos
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
 
 
 const TownTabs = () => {
@@ -165,7 +158,7 @@ const TownTabs = () => {
   
         {maps.map((map, index) => (
           <TabPanel key={index}>
-            <MapContainer center={map.markers[0].position} zoom={13} style={{ height: "400px", width: "100%" }}>
+            <MapContainer center={map.markers[0].position} zoom={13} style={{ height: "400px", width: "100%"}}>
               <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
