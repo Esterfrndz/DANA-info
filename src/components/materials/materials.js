ae94a8e4b10b1f5ee281d8e8769c76e60e8fb793
Ester <esterffernandezz@gmail.com>
2024-11-03 18:17:39 +0100

    updated text

---
+++ src/components/materials/materials.js
@@ -0,0 +1,23 @@
+import React from "react";
+import '../materials/materials.css';
+
+
+
+function materialComponent(){
+
+
+return(
+
+
+<div>
+    
+</div>
+
+)
+
+}
+
+
+export default materialComponent;
+
+
