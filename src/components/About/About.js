896b11f71830c0919c938b0f1ea3546a8ab0eeda
Ester <esterffernandezz@gmail.com>
2024-11-04 19:44:40 +0100

    new add tlfs

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,17 +1,23 @@
 import React from "react";
 import '../About/About.css';
+import { IoIosInformationCircleOutline } from "react-icons/io";
+
 
 function Info(){
 
 return(
 
-
+    
     <div className="info-container">
-
+    {/* <IoIosInformationCircleOutline className="icon-about" /> */}
+    <div className="info-div">
     <h1>Información</h1>
+   <p>En esta web podrás encontrar los puntos de recogida tanto de alimentos cómo de materiales necesarios. <br></br>También podrás ver los materiales más prioritarios y los telefónos de contacto para cualquier tipo de emergencia.</p>
 
-    <p>En esta web podrás encontrar los puntos de recogida tanto de alimentos cómo de materiales necesarios. <br></br>También podrás ver los materiales más prioritarios y los telefónos de contacto para cualquier tipo de emergencia.</p>
-
+   </div> 
+    {/* <IoIosInformationCircleOutline 
+ className="icon-about" /> */}
+    
     </div>
 
 )
--- src/components/tlf/tlf.js
+++ src/components/tlf/tlf.js
@@ -1,5 +1,6 @@
 import React from "react";
 import '../tlf/tlf.css';
+import { Divider } from "@mui/material";
 
 function Tlf(){
 
@@ -10,9 +11,21 @@ function Tlf(){
             <h2 className="title-material-h2">¿Dónde llamar en caso de cualquier emergencia?</h2>
 
         <div className="list-tlf">
-            
+            <h2 className="title-tlfs-h2">Tlfs. Principales</h2>
+            <p>Emergencias <b>112</b></p>
+            <p>Cruz Roja <b>900 104 971</b></p>
+            <p>DGT <b>011</b></p>
             <p>Atención de familiares de las personas desaparecidas: <b>900 365 112</b></p>
             <p>Psicólogas y Psicólogos Sin Fronteras: <b>960 450 230</b></p>
+            <hr></hr>
+            <h2 className="title-tlfs-h2">Tlfs. Personas Desaparecidas</h2>
+            <p>Personas afectadas en La Plana de Utiel-Requena: <b>659 289 726</b></p>
+            <p>Personas afectadas en L'Horta Sud: <b>682 177 249</b></p>
+            <p>Personas afectadas en La Ribera: <b>619 975 059</b></p>
+            <p>Personas afectadas en La Hoya de Buñol: <b>690 027 099</b></p>
+            
+
+
 
         </div>                
 
